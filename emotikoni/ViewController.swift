8067ff317d53267f3bf15b9f3edc4cdf04567035
Aleksa Dordevic <leksapgd@icloud.com>
2018-01-05 16:54:23 +0100

    Initial Commit

---
+++ emotikoni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emotikoni
+//
+//  Created by Aleksa Dordevic on 1/5/18.
+//  Copyright © 2018 Aleksa Dordevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
